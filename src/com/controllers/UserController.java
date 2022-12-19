001643a57c94816469eddf9c3708e6bb5dffdea0
= <=>
2022-12-19 00:33:29 -0800

    add GUI (not finished)

--- src/com/controllers/UserController.java
+++ src/com/controllers/UserController.java
@@ -2,16 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.pengiriman;
-import com.barang.Barang;
+package com.controllers;
 
 /**
  *
  * @author iolux
  */
-public class Pengiriman {
-    private String lokasi;
-    protected Barang barang;
-    
+public class UserController {
     
 }
